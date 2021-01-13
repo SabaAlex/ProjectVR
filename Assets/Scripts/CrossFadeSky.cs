25713d7cf6dd71c1bcc798f5d3931c58de9dc78c
Alexandru Săbăduș <alex.sabadus@optima.ro>
2021-01-13 18:34:42 +0200

    Begin Large Scene Script

---
+++ Assets/Scripts/CrossFadeSky.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossFadeSky : MonoBehaviour
+{
+    private Texture newTexture;
+
+    public float BlendSpeed = 3.0f;
+
+    private bool trigger = false;
+    private float fader = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (true == trigger)
+        {
+            fader += Time.deltaTime * BlendSpeed;
+
+            RenderSettings.skybox.SetFloat("_Blend", fader);
+
+            if (fader >= 1.0f)
+            {
+                trigger = false;
+                fader = 0f;
+
+                RenderSettings.skybox.SetTexture("_MainTex", newTexture);
+                RenderSettings.skybox.SetFloat("_Blend", 0f);
+            }
+        }
+    }
+
+    public void CrossFadeTo(Texture curTexture)
+    {
+        newTexture = curTexture;
+        RenderSettings.skybox.SetTexture("_Texture2", curTexture);
+        trigger = true;
+    }
+}
---
+++ Assets/Scripts/SkyBoxControler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SkyBoxControler : MonoBehaviour {
+
+	public  float CloudsSpeed;
+    public Texture Texture;
+
+    void Start()
+    {
+        CrossFadeSky bt = this.GetComponent<CrossFadeSky>();
+        bt.CrossFadeTo(Texture);
+    }
+
+    void Update()
+	{
+		RenderSettings.skybox.SetFloat("_Rotation", Time.time * CloudsSpeed);
+	}
+}
